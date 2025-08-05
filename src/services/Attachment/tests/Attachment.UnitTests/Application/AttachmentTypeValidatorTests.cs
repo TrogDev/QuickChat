2d3a9ded6ed796ef676d08034210dfdddb8fa92d
TrogDev <vaxnini12345@gmail.com>
2025-08-05 13:41:29 +0300

    Add Attachment unit tests

---
+++ src/services/Attachment/tests/Attachment.UnitTests/Application/AttachmentTypeValidatorTests.cs
@@ -0,0 +1,65 @@
+using QuickChat.Attachment.Application.Exceptions;
+using QuickChat.Attachment.Application.Services;
+using QuickChat.Attachment.Domain.Enums;
+
+namespace QuickChat.Identity.UnitTests.Application;
+
+public class AttachmentTypeValidatorTests
+{
+    private readonly AttachmentTypeValidator validator = new();
+
+    [Fact]
+    public void Validate_WithInvalidImageExtension_ThrowsException()
+    {
+        // Arrange
+        AttachmentType type = AttachmentType.Image;
+        string fileName = "test.mp4";
+
+        // Act & Assert
+        Assert.Throws<InvalidAttachmentTypeException>(() => validator.Validate(type, fileName));
+    }
+
+    [Fact]
+    public void Validate_WithValidImageExtension_DoesNotThrowException()
+    {
+        // Arrange
+        AttachmentType type = AttachmentType.Image;
+        string fileName = "test.jpeg";
+
+        // Act & Assert
+        validator.Validate(type, fileName);
+    }
+
+    [Fact]
+    public void Validate_WithInvalidVideoExtension_ThrowsException()
+    {
+        // Arrange
+        AttachmentType type = AttachmentType.Video;
+        string fileName = "test.pdf";
+
+        // Act & Assert
+        Assert.Throws<InvalidAttachmentTypeException>(() => validator.Validate(type, fileName));
+    }
+
+    [Fact]
+    public void Validate_WithValidVideoExtension_DoesNotThrowException()
+    {
+        // Arrange
+        AttachmentType type = AttachmentType.Video;
+        string fileName = "test.mp4";
+
+        // Act & Assert
+        validator.Validate(type, fileName);
+    }
+
+    [Fact]
+    public void Validate_WithoutExtension_DoesNotThrowException()
+    {
+        // Arrange
+        AttachmentType type = AttachmentType.File;
+        string fileName = "test";
+
+        // Act & Assert
+        validator.Validate(type, fileName);
+    }
+}
